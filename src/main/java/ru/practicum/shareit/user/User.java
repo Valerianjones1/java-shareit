cb8fa8c721991fbe7855c20c041975d7b073b3b1
Valerianjones1 <dinkiev.valery@yandex.ru>
2024-02-25 21:19:10 +0300

    style: Убрал TODO

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -6,9 +6,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-/**
- * TODO Sprint add-controllers.
- */
 @Data
 public class User {
     private Integer id;
