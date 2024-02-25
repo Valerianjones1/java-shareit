8b86e51614535199260aa8e361d3125e4ef7f21b
Valerianjones1 <dinkiev.valery@yandex.ru>
2024-02-25 21:19:33 +0300

    refactor: Убрал неиспользуемые классы

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,27 +1,7 @@
 package ru.practicum.shareit.request;
 
-import lombok.Data;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
-
 /**
  * TODO Sprint add-item-requests.
  */
-@Data
 public class ItemRequest {
-    private final Integer id;
-
-    private final Integer userId;
-
-    @NotNull
-    @NotBlank
-    @Size(max = 200)
-    private String description;
-
-    @NotNull
-    private LocalDateTime created;
-
 }
--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,26 +1,7 @@
 package ru.practicum.shareit.request.dto;
 
-import lombok.Data;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
-
 /**
  * TODO Sprint add-item-requests.
  */
-@Data
 public class ItemRequestDto {
-    private final Integer id;
-
-    private final Integer userId;
-
-    @NotNull
-    @NotBlank
-    @Size(max = 200)
-    private String description;
-
-    @NotNull
-    private LocalDateTime created;
 }
