a0fdaa6de7cfc868dffc29cba27009a36992f76b
Valerianjones1 <dinkiev.valery@yandex.ru>
2024-02-29 02:00:17 +0300

    fix: Убрал валидацию из моделей в dto

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -2,20 +2,12 @@ package ru.practicum.shareit.user;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Data
 public class User {
     private Integer id;
 
-    @NotNull
-    @NotBlank
     private String name;
 
-    @NotNull
-    @Email
     private String email;
 
 }
